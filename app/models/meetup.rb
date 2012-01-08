f9df48d1ac724ba63c1ae5d65ebf6cff9157cde1
Martha Winata <marthawinata@gmail.com>
2012-01-08 21:34:46 +1100

    host user for meetups are now reading from login

--- app/models/meetup.rb
+++ app/models/meetup.rb
@@ -3,5 +3,5 @@ class Meetup < ActiveRecord::Base
   has_many :boardgames, :through => :boardgames_meetups
   has_many :proposed_timings
   has_many :proposed_venue
-  
+  belongs_to :host_user, :class_name => "User",:foreign_key => "host_user_id"
 end
